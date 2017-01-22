1e348074e8d5fb5f273205bf5d33916f40a24e3f
Malte Hübner <maltehuebner@gmx.org>
2017-01-22 23:48:42 +0100

    WIP for FetchCommand.

---
+++ src/AppBundle/Command/FetchCommand.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\SourceFetcher\Query\UbNO2Query;
+use AppBundle\SourceFetcher\Query\UbO3Query;
+use AppBundle\SourceFetcher\Query\UbPm10Query;
+use AppBundle\SourceFetcher\Query\UbSO2Query;
+use AppBundle\SourceFetcher\SourceFetcher;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class FetchCommand extends ContainerAwareCommand
+{
+
+    protected function configure()
+    {
+        $this
+            ->setName('luft:fetch')
+            ->setDescription('')
+            ->addOption('pm10')
+            ->addOption('so2')
+            ->addOption('no2')
+            ->addOption('o3')
+            ->addArgument('dateTime', InputArgument::OPTIONAL);
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if ($input->getArgument('dateTime')) {
+            $dateTime = new \DateTime($input->getArgument('dateTime'));
+        } else {
+            $dateTime = new \DateTime('2017-01-15 12:00:00');
+        }
+
+        if ($input->getOption('pm10')) {
+            $this->fetchPM10($dateTime);
+        }
+
+        if ($input->getOption('so2')) {
+            $this->fetchSO2($dateTime);
+        }
+
+        if ($input->getOption('no2')) {
+            $this->fetchNO2($dateTime);
+        }
+
+        if ($input->getOption('o3')) {
+            $this->fetchO3($dateTime);
+        }
+
+    }
+
+    protected function fetchPM10(\DateTime $dateTime)
+    {
+        $query = new UbPM10Query($dateTime);
+
+        $sourceFetcher = new SourceFetcher();
+        $sourceFetcher->query($query);
+    }
+
+    protected function fetchSO2(\DateTime $dateTime)
+    {
+        $query = new UbSO2Query($dateTime);
+
+        $sourceFetcher = new SourceFetcher();
+        $sourceFetcher->query($query);
+    }
+
+    protected function fetchNO2(\DateTime $dateTime)
+    {
+        $query = new UbNO2Query($dateTime);
+
+        $sourceFetcher = new SourceFetcher();
+        $sourceFetcher->query($query);
+    }
+
+    protected function fetchO3(\DateTime $dateTime)
+    {
+        $query = new UbO3Query($dateTime);
+
+        $sourceFetcher = new SourceFetcher();
+        $sourceFetcher->query($query);
+    }
+}
