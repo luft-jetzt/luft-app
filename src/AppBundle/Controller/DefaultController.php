138ce7a7c98551c0ac2670fb8fb10ebce2a75f23
Malte Hübner <maltehuebner@gmx.org>
2017-01-24 22:19:23 +0100

    WIP for coords at Default Front Page.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Caldera\GeoBasic\Coord\Coord;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,11 +14,28 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $latitude = $request->query->get('latitude');
+        $longitude = $request->query->get('longitude');
+
+        if (!$latitude || !$longitude) {
+            throw $this->createNotFoundException();
+        }
+
+        $coord = new Coord(
+            $latitude,
+            $longitude
+        );
+
         return $this->render(
             'AppBundle:Default:index.html.twig',
             [
 
             ]
         );
+    }
+
+    protected function findNearestStation(string $pollutant, Coord $coord)
+    {
+
     }
 }
