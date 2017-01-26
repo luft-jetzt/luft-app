1be7086f457b84143d242197bd3439210aa8fc7d
Malte Hübner <maltehuebner@gmx.org>
2017-01-26 01:30:19 +0100

    Add PollutionLevel.

---
+++ src/AppBundle/PollutionLevel/PollutionLevel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AppBundle\PollutionLevel;
+
+class PollutionLevel
+{
+    const LEVEL_ACCEPTABLE = 1;
+    const LEVEL_WARNING = 2;
+    const LEVEL_DANGER = 3;
+    const LEVEL_DEATH = 4;
+}
