9de9e49fb3a29a49d17298a2a31ff5fe8fdb893e
Malte Hübner <maltehuebner@gmx.org>
2017-01-29 19:57:30 +0100

    Constants go to PollutantInterface.

--- src/AppBundle/Pollution/Pollutant/AbstractPollutant.php
+++ src/AppBundle/Pollution/Pollutant/AbstractPollutant.php
@@ -4,9 +4,5 @@ namespace AppBundle\Pollution\Pollutant;
 
 abstract class AbstractPollutant implements PollutantInterface
 {
-    const POLLUTANT_PM10 = 1;
-    const POLLUTANT_O3 = 2;
-    const POLLUTANT_NO2 = 3;
-    const POLLUTANT_SO2 = 4;
-    const POLLUTANT_CO = 5;
+
 }
--- src/AppBundle/Pollution/Pollutant/PollutantInterface.php
+++ src/AppBundle/Pollution/Pollutant/PollutantInterface.php
@@ -4,6 +4,12 @@ namespace AppBundle\Pollution\Pollutant;
 
 interface PollutantInterface
 {
+    const POLLUTANT_PM10 = 1;
+    const POLLUTANT_O3 = 2;
+    const POLLUTANT_NO2 = 3;
+    const POLLUTANT_SO2 = 4;
+    const POLLUTANT_CO = 5;
+
     public function getUnit(): string;
     public function getName(): string;
 }
