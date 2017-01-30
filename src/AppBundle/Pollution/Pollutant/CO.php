129a369930a0c1aa48c4f28842ab452d010e7849
Malte Hübner <maltehuebner@gmx.org>
2017-01-30 22:18:46 +0100

    Fix CO data unit.

--- src/AppBundle/Pollution/Pollutant/CO.php
+++ src/AppBundle/Pollution/Pollutant/CO.php
@@ -8,7 +8,7 @@ class CO extends AbstractPollutant
 {
     public function getUnit(): string
     {
-        return 'mg/m<sup>3</sup>';
+        return 'µg/m<sup>3</sup>';
     }
 
     public function getName(): string
