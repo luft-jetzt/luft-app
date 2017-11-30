8c31fa625e27e332387bf1262772c072f52ddd5a
Malte Hübner <maltehuebner@gmx.org>
2017-11-30 13:46:27 +0100

    Copy ApiController.

---
+++ src/AppBundle/Controller/ApiController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiController extends AbstractController
+{
+    /**
+     * @Route("/api/{stationCode}", name="api_station", options = { "expose" = true })
+     */
+    public function stationAction(Request $request, string $stationCode): Response
+    {
+        $station = $this->getDoctrine()->getRepository('AppBundle:Station')->findOneByStationCode($stationCode);
+
+        if (!$station) {
+            throw $this->createNotFoundException();
+        }
+
+        $dataList = $this->getDataListFromStationList([$station]);
+
+        $boxList = $this->getBoxListFromDataList($dataList);
+
+        $boxList = $this->decorateBoxList($boxList);
+
+        return $this->render(
+            'AppBundle:Default:station.html.twig',
+            [
+                'station' => $station,
+                'boxList' => $boxList
+            ]
+        );
+    }
+
+    /**
+     * @Route("/api", name="api_display", options = { "expose" = true })
+     */
+    public function displayAction(Request $request): Response
+    {
+        $coord = $this->getCoordByRequest($request);
+
+        if (!$coord) {
+            return $this->render('AppBundle:Default:select.html.twig');
+        }
+
+        $stationList = $this->findNearestStations($coord);
+
+        if (0 === count($stationList)) {
+            return $this->noStationAction($request, $coord);
+        }
+
+        $dataList = $this->getDataListFromStationList($stationList);
+
+        $boxList = $this->getBoxListFromDataList($dataList);
+
+        $boxList = $this->decorateBoxList($boxList);
+
+        return $this->render(
+            'AppBundle:Default:display.html.twig',
+            [
+                'boxList' => $boxList
+            ]
+        );
+    }
+}
