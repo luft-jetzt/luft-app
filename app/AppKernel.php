075090b96d3f5609f612b2a03a28eb0e50df0ee7
Malte Hübner <maltehuebner@gmx.org>
2017-12-01 12:49:33 +0100

    Forgot to add SonataDoctrineORMAdminBundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,7 @@ class AppKernel extends Kernel
             new Sonata\BlockBundle\SonataBlockBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Sonata\AdminBundle\SonataAdminBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
             new AppBundle\AppBundle(),
         ];
 
