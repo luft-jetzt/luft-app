e14effc62e9aa2375f8ec27779895f9380b9b331
Malte Hübner <maltehuebner@gmx.org>
2017-12-04 14:04:02 +0100

    More BoxDecorator stuff.

--- src/AppBundle/Pollution/BoxDecorator/BoxDecorator.php
+++ src/AppBundle/Pollution/BoxDecorator/BoxDecorator.php
@@ -3,24 +3,9 @@
 namespace AppBundle\Pollution\BoxDecorator;
 
 use AppBundle\Pollution\Box\Box;
-use AppBundle\Pollution\Pollutant\CO;
-use AppBundle\Pollution\Pollutant\NO2;
-use AppBundle\Pollution\Pollutant\O3;
-use AppBundle\Pollution\Pollutant\PM10;
-use AppBundle\Pollution\Pollutant\PollutantInterface;
-use AppBundle\Pollution\Pollutant\SO2;
 
-class BoxDecorator implements BoxDecoratorInterface
+class BoxDecorator extends AbstractBoxDecorator
 {
-    protected $boxList = [];
-
-    public function setBoxList(array $boxList): BoxDecoratorInterface
-    {
-        $this->boxList = $boxList;
-
-        return $this;
-    }
-
     public function decorate(): BoxDecoratorInterface
     {
         /** @var Box $box */
@@ -38,21 +23,5 @@ class BoxDecorator implements BoxDecoratorInterface
         }
 
         return $this;
-    }
-
-    public function getBoxList(): array
-    {
-        return $this->boxList;
-    }
-
-    protected function getPollutantById(int $pollutantId): PollutantInterface
-    {
-        switch ($pollutantId) {
-            case 1: return new PM10();
-            case 2: return new O3();
-            case 3: return new NO2();
-            case 4: return new SO2();
-            case 5: return new CO();
-        }
     }
 }
