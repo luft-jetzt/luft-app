a34f3519f7e65ae9dc7a689efbda1d3f03cef69c
Malte Hübner <maltehuebner@gmx.org>
2017-12-04 14:19:42 +0100

    Populate interface.

--- src/AppBundle/Pollution/BoxDecorator/BoxDecoratorInterface.php
+++ src/AppBundle/Pollution/BoxDecorator/BoxDecoratorInterface.php
@@ -4,4 +4,7 @@ namespace AppBundle\Pollution\BoxDecorator;
 
 interface BoxDecoratorInterface
 {
+    public function setBoxList(array $boxList): BoxDecoratorInterface;
+    public function getBoxList(): array;
+    public function decorate(): BoxDecoratorInterface;
 }
