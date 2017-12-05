c5154cfb7f63669a20e87a6518053421290784c3
Malte Hübner <maltehuebner@gmx.org>
2017-12-06 00:42:44 +0100

    Click on marker opens station page.

--- src/AppBundle/Resources/public/js/map.js
+++ src/AppBundle/Resources/public/js/map.js
@@ -35,7 +35,7 @@ function createMap(id, latitude, longitude) {
 }
 
 function createCityMap(id) {
-    var map = L.map(id).setView([10, 55], 13);
+    var map = L.map(id);
 
     L.tileLayer('https://api.mapbox.com/styles/v1/maltehuebner/ciz8okvlo001f2spm3bbya1kd/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFsdGVodWVibmVyIiwiYSI6IjB5c2QtNXcifQ.I7OHZr0wtAvqE0wIY_psfg', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
@@ -44,14 +44,17 @@ function createCityMap(id) {
     var markerGroup = new L.featureGroup();
 
     $('.station').each(function (index) {
-        var id = $(this).prop('id');
+        var stationCode = $(this).data('station-code');
         var latitude = $(this).data('latitude');
         var longitude = $(this).data('longitude');
 
         var marker = L.marker([latitude, longitude]).addTo(markerGroup);
+
+        marker.on('click', function() {
+            window.location = Routing.generate('station', { stationCode: stationCode });
+        });
     });
 
     markerGroup.addTo(map);
     map.fitBounds(markerGroup.getBounds());
-
 }
