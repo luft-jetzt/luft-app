ab8a1436cab856e45d62638006ede1f072e92eb0
Malte Hübner <maltehuebner@gmx.org>
2017-12-09 17:41:16 +0100

    WIP for implementing city list.

---
+++ src/AppBundle/Controller/TemplateController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\City;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TemplateController extends AbstractController
+{
+    public function cityListAction(Request $request): Response
+    {
+        return $this->render(
+            'AppBundle:Template:city_list.html.twig', [
+                'cityList' => $this->getDoctrine()->getRepository(City::class)->findBy([], ['name' => 'ASC']),
+            ]
+        );
+    }
+}
