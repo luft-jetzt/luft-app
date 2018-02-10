3ce13d04f1e82503833b0d8dac4da9151182f7cf
Malte Hübner <maltehuebner@gmx.org>
2018-02-10 14:33:28 +0100

    WIP.

--- src/Caldera/StaticmapBundle/Twig/Extension/StaticmapsTwigExtension.php
+++ src/Caldera/StaticmapBundle/Twig/Extension/StaticmapsTwigExtension.php
@@ -2,8 +2,7 @@
 
 namespace Caldera\StaticmapBundle\Twig\Extension;
 
-use Criticalmass\Bundle\AppBundle\Entity\City;
-use Criticalmass\Bundle\AppBundle\Entity\Ride;
+use Caldera\GeoBasic\Coord\CoordInterface;
 
 class StaticmapsTwigExtension extends \Twig_Extension
 {
@@ -29,41 +28,20 @@ class StaticmapsTwigExtension extends \Twig_Extension
         ];
     }
 
-    public function staticmaps($object, int $width = null, int $height = null, int $zoom = null): string
+    public function staticmaps(CoordInterface $object, int $width = null, int $height = null, int $zoom = null): string
     {
-        if ($object instanceof Ride) {
-            return $this->staticmapsRide($object, $width, $height, $zoom);
-        } elseif ($object instanceof City) {
-            return $this->staticmapsCity($object, $width, $height, $zoom);
-        }
+        $parameters = [
+            'center' => sprintf('%f,%f', $object->getLatitude(), $object->getLongitude()),
+            'markers' => sprintf('%f,%f,%s,%s,%s', $object->getLatitude(), $object->getLongitude(), 'circle', 'blue', 'flag'),
 
-        return '';
+        ];
+
+        return $this->generateMapUrl($parameters, $width, $height, $zoom);
     }
 
     public function getName(): string
     {
         return 'staticmaps_extension';
-    }
-
-    public function staticmapsRide(Ride $ride, int $width = null, int $height = null, int $zoom = null): string
-    {
-        $parameters = [
-            'center' => sprintf('%f,%f', $ride->getLatitude(), $ride->getLongitude()),
-            'markers' => sprintf('%f,%f,%s,%s,%s', $ride->getLatitude(), $ride->getLongitude(), 'circle', 'red', 'bicycle'),
-        ];
-
-        return $this->generateMapUrl($parameters, $width, $height, $zoom);
-    }
-
-    public function staticmapsCity(City $city, int $width = null, int $height = null, int $zoom = null): string
-    {
-        $parameters = [
-            'center' => sprintf('%f,%f', $city->getLatitude(), $city->getLongitude()),
-            'markers' => sprintf('%f,%f,%s,%s,%s', $city->getLatitude(), $city->getLongitude(), 'circle', 'blue', 'university'),
-
-        ];
-
-        return $this->generateMapUrl($parameters, $width, $height, $zoom);
     }
 
     protected function generateMapUrl(array $parameters = [], int $width = null, int $height = null, int $zoom = null): string
@@ -80,7 +58,7 @@ class StaticmapsTwigExtension extends \Twig_Extension
 
         $parameters = array_merge($parameters, $this->defaultParameters, $viewParameters);
 
-        return sprintf('%sstaticmap.php?%s', $this->staticmapsHost, $this->generateMapUrlParameters($parameters));
+        return sprintf('%s/staticmap.php?%s', $this->staticmapsHost, $this->generateMapUrlParameters($parameters));
     }
 
     protected function generateMapUrlParameters(array $parameters = []): string
