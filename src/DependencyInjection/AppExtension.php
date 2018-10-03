203dda8b6d78679069a8ce1f6216753ad05b5987
Malte Hübner <maltehuebner@gmx.org>
2018-10-03 22:47:29 +0200

    Introduce more Dependency injection stuff.

---
+++ src/DependencyInjection/AppExtension.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace App\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class AppExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+    }
+}
---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace App\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('caldera');
+
+        return $treeBuilder;
+    }
+}
