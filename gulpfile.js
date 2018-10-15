53dacdb58b55f5c6d601f902303dcda14e65aa11
Malte Hübner <maltehuebner@gmx.org>
2018-10-15 19:34:46 +0200

    fonts

--- gulpfile.js
+++ gulpfile.js
@@ -13,10 +13,16 @@ gulp.task('copy-asset-images', function () {
         .pipe(gulp.dest('public/img/'));
 });
 
+gulp.task('copy-fonts', function () {
+    return gulp.src('node_modules/font-awesome/fonts/*')
+        .pipe(gulp.dest('public/fonts'));
+});
+
 gulp.task('compress-css', function () {
     return gulp.src([
             'node_modules/leaflet/dist/leaflet.css',
             'node_modules/bootstrap/dist/css/bootstrap.css',
+            'node_modules/font-awesome/css/font-awesome.css',
             'assets/css/*/*',
         ])
         .pipe(cleanCSS())
@@ -36,6 +42,6 @@ gulp.task('compress-js', function () {
         .pipe(gulp.dest('public/js/'));
 });
 
-gulp.task('build', ['copy-images', 'copy-asset-images', 'compress-js', 'compress-css'], function () {});
+gulp.task('build', ['copy-images', 'copy-asset-images', 'copy-fonts', 'compress-js', 'compress-css'], function () {});
 
 gulp.task('default', ['build']);
