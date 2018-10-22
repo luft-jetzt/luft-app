498aee54b634303534a467cf3ab1decedcdf6fff
Malte Hübner <maltehuebner@gmx.org>
2018-10-22 07:37:56 +0200

    WIP for templating.

--- assets/js/typeahead.js
+++ assets/js/typeahead.js
@@ -12,14 +12,36 @@ $(document).ready(function () {
         }
     });
 
-    $('.typeahead').typeahead(null, {
+    $('.typeahead').typeahead({
+        hint: false,
+        highlight: true,
+        minLength: 3,
+        classNames: {
+            dataset: 'tt-dataset tt-dataset-results container'
+        }
+    }, {
         name: 'cities',
         source: cities,
         displayKey: 'value',
         templates: {
-            suggestion: function (city) {
-                return '<p>' + city.value + '</p>';
-            }
+            suggestion: renderRideSuggestion,
         }
     });
 });
+
+function renderRideSuggestion(data) {
+    var html = '';
+
+    html += '<a href="' + data.url + '">';
+
+    html += '<div class="row">';
+    html += '<div class="col-12">';
+    html += '<i class="fa fa-university"></i> ';
+    html += data.value;
+    html += '</div>';
+    html += '</div>';
+
+    html += '</a>';
+
+    return html;
+}
