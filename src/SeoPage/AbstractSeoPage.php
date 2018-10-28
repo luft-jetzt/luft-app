13cd66caeb555b1b5b4e9aa1a578efd53b5c33e5
Malte Hübner <maltehuebner@gmx.org>
2018-10-28 14:50:48 +0100

    Fix preview images without full filename.

--- src/SeoPage/AbstractSeoPage.php
+++ src/SeoPage/AbstractSeoPage.php
@@ -3,6 +3,10 @@
 namespace App\SeoPage;
 
 use Sonata\SeoBundle\Seo\SeoPageInterface as SonataSeoPageInterface;
+use Symfony\Component\Asset\Package;
+use Symfony\Component\Asset\VersionStrategy\StaticVersionStrategy;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 abstract class AbstractSeoPage implements SeoPageInterface
 {
@@ -12,9 +16,27 @@ abstract class AbstractSeoPage implements SeoPageInterface
     /** @var string $assetsVersion */
     protected $assetsVersion;
 
-    public function __construct(SonataSeoPageInterface $sonataSeoPage, string $assetsVersion)
+    /** @var RequestStack $requestStack */
+    protected $requestStack;
+
+    public function __construct(SonataSeoPageInterface $sonataSeoPage, RequestStack $requestStack, string $assetsVersion)
     {
         $this->sonataSeoPage = $sonataSeoPage;
+        $this->requestStack = $requestStack;
         $this->assetsVersion = $assetsVersion;
+    }
+
+    protected function getRequest(): Request
+    {
+        return $this->requestStack->getCurrentRequest();
+    }
+
+    protected function asset(string $filename): string
+    {
+        $host = $this->getRequest()->getSchemeAndHttpHost();
+
+        $package = new Package(new StaticVersionStrategy($this->assetsVersion));
+
+        return $package->getUrl(sprintf('%s%s', $host, '/img/share/opengraph.jpeg'));
     }
 }
