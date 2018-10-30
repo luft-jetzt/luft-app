dc092ce0ac44a9e5b386414949ae7ea85b594b8c
Malte Hübner <maltehuebner@gmx.org>
2018-10-30 08:07:59 +0100

    Same for color names needed by extramarkers.

--- src/Twig/Extension/PollutionLevelTwigExtension.php
+++ src/Twig/Extension/PollutionLevelTwigExtension.php
@@ -14,16 +14,32 @@ class PollutionLevelTwigExtension extends \Twig_Extension
         6 => '#dc3545',
     ];
 
+    protected $backgroundColorNames = [
+        0 => 'white',
+        1 => 'green',
+        2 => 'green',
+        3 => 'yellow',
+        4 => 'yellow',
+        5 => 'red',
+        6 => 'red',
+    ];
+
     public function getFunctions(): array
     {
         return [
             new \Twig_SimpleFunction('pollution_color', [$this, 'pollutionColor'], ['is_safe' => ['raw']]),
+            new \Twig_SimpleFunction('pollution_color_name', [$this, 'pollutionColorName'], ['is_safe' => ['raw']]),
         ];
     }
 
     public function pollutionColor(int $pollutionLevel): string
     {
         return $this->backgroundColors[$pollutionLevel];
+    }
+
+    public function pollutionColorName(int $pollutionLevel): string
+    {
+        return $this->backgroundColorNames[$pollutionLevel];
     }
 
     public function getName(): string
