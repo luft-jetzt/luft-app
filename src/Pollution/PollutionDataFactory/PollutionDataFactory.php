674526026eee60bcf8e37cf6dd7eaa39b45a717d
Malte Hübner <maltehuebner@gmx.org>
2018-11-05 13:23:41 +0100

    Refactoring pollution data factory.

--- src/Pollution/PollutionDataFactory/PollutionDataFactory.php
+++ src/Pollution/PollutionDataFactory/PollutionDataFactory.php
@@ -2,57 +2,10 @@
 
 namespace App\Pollution\PollutionDataFactory;
 
-use App\Entity\Station;
 use App\Pollution\Box\Box;
-use App\Pollution\BoxDecorator\BoxDecoratorInterface;
-use App\Pollution\DataList\DataList;
-use App\Pollution\DataRetriever\DataRetrieverInterface;
-use App\Pollution\StationFinder\StationFinderInterface;
-use Caldera\GeoBasic\Coord\CoordInterface;
 
-class PollutionDataFactory
+class PollutionDataFactory extends AbstractPollutionDataFactory
 {
-    /** @var CoordInterface $coord */
-    protected $coord;
-
-    /** @var StationFinderInterface $stationFinder */
-    protected $stationFinder;
-
-    /** @var StationFinderInterface $boxDecorator */
-    protected $boxDecorator;
-
-    /** @var DataList $dataList */
-    protected $dataList;
-
-    /** @var DataRetrieverInterface */
-    protected $dataRetriever;
-
-    protected $stationList = [];
-
-    public function __construct(StationFinderInterface $stationFinder, BoxDecoratorInterface $boxDecorator, DataRetrieverInterface $dataRetriever)
-    {
-        $this->stationFinder = $stationFinder;
-        $this->dataList = new DataList();
-        $this->boxDecorator = $boxDecorator;
-        $this->dataRetriever = $dataRetriever;
-    }
-
-    public function setCoord(CoordInterface $coord): PollutionDataFactory
-    {
-        $this->coord = $coord;
-        $this->stationList = $this->stationFinder->setCoord($this->coord)->findNearestStations();
-
-        return $this;
-    }
-
-    public function setStation(Station $station): PollutionDataFactory
-    {
-        $this->coord = $station;
-        $this->stationList = [$station];
-
-        return $this;
-    }
-
     public function createDecoratedBoxList(): array
     {
         $dataList = $this->getDataListFromStationList($this->stationList);
@@ -103,12 +56,5 @@ class PollutionDataFactory
             ->decorate()
             ->getBoxList()
         ;
-    }
-
-    protected function reset(): PollutionDataFactory
-    {
-        $this->dataList->reset();
-
-        return $this;
     }
 }
