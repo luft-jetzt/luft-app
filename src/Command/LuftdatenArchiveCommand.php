4474ff79718d35e724968838e35b7d64528a2f87
Malte Hübner <maltehuebner@gmx.org>
2018-11-11 19:56:11 +0100

    WIP.

--- src/Command/LuftdatenArchiveCommand.php
+++ src/Command/LuftdatenArchiveCommand.php
@@ -10,6 +10,7 @@ use App\Provider\Luftdaten\SourceFetcher\Parser\Parser;
 use App\Provider\Luftdaten\SourceFetcher\SourceFetcher;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -18,9 +19,17 @@ class LuftdatenArchiveCommand extends ContainerAwareCommand
     /** @var ArchiveFetcher $archiveFetcher */
     protected $archiveFetcher;
 
-    public function __construct(?string $name = null, ArchiveFetcher $archiveFetcher)
+    /** @var UniquePersisterInterface $uniquePersister */
+    protected $uniquePersister;
+
+    /** @var LuftdatenProvider $provider */
+    protected $provider;
+
+    public function __construct(?string $name = null, ArchiveFetcher $archiveFetcher, UniquePersisterInterface $uniquePersister, LuftdatenProvider $luftdatenProvider)
     {
         $this->archiveFetcher = $archiveFetcher;
+        $this->uniquePersister = $uniquePersister;
+        $this->provider = $luftdatenProvider;
 
         parent::__construct($name);
     }
@@ -29,13 +38,18 @@ class LuftdatenArchiveCommand extends ContainerAwareCommand
     {
         $this
             ->setName('luft:luftdaten-archive')
-            ->setDescription('');
+            ->setDescription('')
+            ->addArgument('date', InputArgument::REQUIRED, 'Date of data to fetch');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): void
     {
-        $this->archiveFetcher
+        $valueList = $this->archiveFetcher
             ->setDateTime(new \DateTime('2018-11-01'))
             ->fetch();
+
+        $this->uniquePersister
+            ->setProvider($this->provider)
+            ->persistValues($valueList);
     }
 }
--- src/Provider/Luftdaten/SourceFetcher/ArchiveFetcher/ArchiveFetcher.php
+++ src/Provider/Luftdaten/SourceFetcher/ArchiveFetcher/ArchiveFetcher.php
@@ -2,6 +2,7 @@
 
 namespace App\Provider\Luftdaten\SourceFetcher\ArchiveFetcher;
 
+use App\Pollution\Pollutant\PollutantInterface;
 use App\Pollution\Value\Value;
 use Curl\Curl;
 use League\Csv\Reader;
@@ -56,30 +57,105 @@ class ArchiveFetcher
         return (string) $this->curl->response;
     }
 
-    protected function parseFile(string $csvFileContent): Value
+    protected function checkHeaderColumns(array $headerColumns): bool
     {
+        $requiredColumns = [
+            'timestamp',
+            'location',
+            'P1',
+            'P2',
+        ];
+
+        $result = true;
+
+        foreach ($requiredColumns as $requiredColumn) {
+            if (!in_array($requiredColumn, $headerColumns)) {
+                $result = false;
+
+                break;
+            }
+        }
+
+        return $result;
+    }
+
+    protected function checkSensorName(string $csvFilename): bool
+    {
+        $acceptedSensorNames = [
+            'pms5003_sensor',
+            'pms7003_sensor',
+            'sds011_sensor',
+            'ppd42ns_sensor',
+            'hpm_sensor',
+        ];
+
+        $result = false;
+
+        foreach ($acceptedSensorNames as $acceptedSensorName) {
+            if (false !== strpos($csvFilename, $acceptedSensorName)) {
+                $result = true;
+
+                break;
+            }
+        }
+
+        return $result;
+    }
+
+    protected function parseFile(string $csvFileContent): array
+    {
+        $valueList = [];
+
         $csv = Reader::createFromString(utf8_decode($csvFileContent));
 
         $csv
             ->setDelimiter(';')
             ->setHeaderOffset(0);
 
-        foreach ($csv as $dataLine) {
-            var_dump($dataLine);
+        if (!$this->checkHeaderColumns($csv->getHeader())) {
+            return [];
         }
 
-        die;
+        foreach ($csv as $dataLine) {
+            $dateTime = new \DateTime($dataLine['timestamp']);
+            $stationCode = sprintf('LFTDTN%d', $dataLine['location']);
+
+            $pm10Value = new Value();
+            $pm10Value->setPollutant(PollutantInterface::POLLUTANT_PM10)
+                ->setDateTime($dateTime)
+                ->setStation($stationCode)
+                ->setValue((float) $dataLine['P1']);
+
+            $pm25Value = new Value();
+            $pm25Value->setPollutant(PollutantInterface::POLLUTANT_PM25)
+                ->setDateTime($dateTime)
+                ->setStation($stationCode)
+                ->setValue((float) $dataLine['P2']);
+
+            $valueList[] = $pm10Value;
+            $valueList[] = $pm25Value;
+        }
+
+        return $valueList;
     }
 
-    public function fetch(): void
+    public function fetch(): array
     {
+        $valueList = [];
+
         $csvLinks = $this->fetchStationCsvFiles();
 
         foreach ($csvLinks as $csvLink) {
+            if (!$this->checkSensorName($csvLink)) {
+                return [];
+            }
+
             $csvFileContent = $this->loadCsvContent($csvLink);
 
-            $this->parseFile($csvFileContent);
+            $valueList = array_merge($this->parseFile($csvFileContent), $valueList);
         }
+
+        return $valueList;
     }
 
 }
