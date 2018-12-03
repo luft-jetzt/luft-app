e3c2635601bdc9d26fe4ee794dd36f915a195837
Malte Hübner <maltehuebner@gmx.org>
2018-12-03 22:58:40 +0100

    Finally get done with those aggregations!!!

--- src/Analysis/KomfortofenAnalysis/KomfortofenAnalysis.php
+++ src/Analysis/KomfortofenAnalysis/KomfortofenAnalysis.php
@@ -62,11 +62,11 @@ class KomfortofenAnalysis implements KomfortofenAnalysisInterface
 
         /** @var array $bucket */
         foreach ($buckets as $bucket) {
-            if (array_key_exists('derivative', $bucket)) {
+            if (array_key_exists('derivative_agg', $bucket)) {
                 $resultList[] = [
                     'dateTime' => $bucket['key_as_string'],
                     'value' => array_pop($bucket['max_agg']),
-                    'derivative' => array_pop($bucket['derivative'])
+                    'derivative' => array_pop($bucket['derivative_agg'])
                 ];
             }
         }
@@ -79,20 +79,19 @@ class KomfortofenAnalysis implements KomfortofenAnalysisInterface
         $stationQuery = new \Elastica\Query\Term(['station' => $this->station->getId()]);
         $pollutantQuery = new \Elastica\Query\Term(['pollutant' => 1]);
 
-        $boolQuery = new \Elastica\Query\BoolQuery();
-        $boolQuery
-            ->addMust($pollutantQuery)
-            ->addMust($stationQuery);
-
         $dateTimeQuery = new \Elastica\Query\Range('dateTime', [
             'gt' => $this->fromDateTime->format('Y-m-d H:i:s'),
             'lte' => $this->untilDateTime->format('Y-m-d H:i:s'),
             'format' => 'yyyy-MM-dd HH:mm:ss'
         ]);
 
-        $boolQuery->addMust($dateTimeQuery);
+        $boolQuery = new \Elastica\Query\BoolQuery();
+        $boolQuery
+            ->addMust($pollutantQuery)
+            ->addMust($stationQuery)
+            ->addMust($dateTimeQuery);
 
-        $histogram = new \Elastica\Aggregation\DateHistogram('value_agg', 'dateTime', '1H');
+        $histogram = new \Elastica\Aggregation\DateHistogram('histogram_agg', 'dateTime', '1H');
         $histogram->setTimezone('Europe/Berlin');
         $histogram->setFormat('yyyy-MM-dd HH:mm:ss');
 
@@ -100,17 +99,25 @@ class KomfortofenAnalysis implements KomfortofenAnalysisInterface
         $max->setField('value');
         $histogram->addAggregation($max);
 
-        $derive = new \Elastica\Aggregation\Derivative('derivative');
+        $derive = new \Elastica\Aggregation\Derivative('derivative_agg');
         $derive->setBucketsPath('max_agg');
         $histogram->addAggregation($derive);
 
+        $bucketSelector = new \Elastica\Aggregation\BucketSelector('bucket_selector');
+        $bucketSelector->setBucketsPath(['my_var' => 'derivative_agg']);
+        $bucketSelector->setGapPolicy('skip');
+        $bucketSelector->setScript('params.my_var != null && params.my_var > 10');
+        $histogram->addAggregation($bucketSelector);
+
         $query = new \Elastica\Query($boolQuery);
         $query->addAggregation($histogram);
+
+        //echo json_encode($query->toArray());
 
         $results = $this->finder->findPaginated($query);
 
         $buckets = $results->getAdapter()->getAggregations();
 
-        return $this->convertToList($buckets['value_agg']['buckets']);
+        return $this->convertToList($buckets['histogram_agg']['buckets']);
     }
 }
