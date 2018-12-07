339f131f2932458f8043cac4d8c6d0394659a822
Malte Hübner <maltehuebner@gmx.org>
2018-12-07 13:44:54 +0100

    More seo for analysis.

--- src/Controller/AnalysisController.php
+++ src/Controller/AnalysisController.php
@@ -3,11 +3,12 @@
 namespace App\Controller;
 
 use App\Analysis\KomfortofenAnalysis\KomfortofenAnalysisInterface;
+use App\SeoPage\SeoPageInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class AnalysisController extends AbstractController
 {
-    public function komfortofenAction(KomfortofenAnalysisInterface $komfortofenAnalysis): Response
+    public function komfortofenAction(KomfortofenAnalysisInterface $komfortofenAnalysis, SeoPageInterface $seoPage): Response
     {
         $interval = new \DateInterval('P30D');
         $untilDateTime = new \DateTimeImmutable();
@@ -18,6 +19,8 @@ class AnalysisController extends AbstractController
             ->setUntilDateTime($untilDateTime);
 
         $ofens = $komfortofenAnalysis->analyze();
+
+        $seoPage->setTitle('Finde Feinstaub-Belastungen aus Holzöfen in deiner Nähe');
 
         return $this->render('Analysis/komfortofen.html.twig', [
             'ofenList' => $ofens,
