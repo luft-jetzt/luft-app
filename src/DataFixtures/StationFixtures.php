3a126edb75eecf74604d5e4a4129193966e8cf7f
Malte Hübner <maltehuebner@gmx.org>
2019-07-03 08:27:13 +0200

    Add more stations.

--- src/DataFixtures/StationFixtures.php
+++ src/DataFixtures/StationFixtures.php
@@ -13,8 +13,24 @@ class StationFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $this->createStation(54.319766, 10.123092, 17, 'DESH007', StationType::BACKGROUND, AreaType::URBAN, 'uba_de', 'Kiel-Schützenwall  Verk.', null, new \DateTime('1992-06-16'), new \DateTime('2000-05-11'));
+        $this->createStation(54.319766, 10.123092, 17, 'DESH007', StationType::BACKGROUND, AreaType::URBAN, 'uba_de', 'Kiel-Schützenwall Verk.', null, new \DateTime('1992-06-16'), new \DateTime('2000-05-11'));
+
         $this->createStation(54.344196, 10.10438, 25, 'DESH012', StationType::BACKGROUND, AreaType::SUBURBAN, 'uba_de', 'Kiel-Stadtrand', null, new \DateTime('1985-07-03'), new \DateTime('1999-06-25'));
+
+        $this->createStation(54.326394, 10.115586, 25, 'DESH019', StationType::TRAFFIC, AreaType::URBAN, 'uba_de', 'Kiel-Westring Verk.', null, new \DateTime('1995-04-26'), new \DateTime('2005-05-09'));
+
+        $this->createStation(54.332679, 10.136176, 10, 'DESH019', StationType::BACKGROUND, AreaType::URBAN, 'uba_de', 'Kiel Schauenburger Str.', null, new \DateTime('2000-05-11'), new \DateTime('2006-11-30'));
+
+        $this->createStation(54.34761, 10.1056, 30, 'DESH057', StationType::BACKGROUND, AreaType::URBAN, 'uba_de', 'Kiel-Bremerskamp', null, new \DateTime('2017-06-22'), null);
+
+        $this->createStation(54.304676, 10.135158, 8, 'DESH027', StationType::TRAFFIC, AreaType::URBAN, 'uba_de', 'Kiel-Bahnhofstr. Verk.', null, new \DateTime('2005-05-11'), null);
+
+        $this->createStation(54.309674, 10.116563, 29, 'DESH052', StationType::BACKGROUND, AreaType::URBAN, 'uba_de', 'Kiel-Max-Planck-Str.', null, new \DateTime('2007-04-25'), new \DateTime('2017-06-22'));
+
+        $this->createStation(54.304242, 10.122364, 18, 'DESH033', StationType::TRAFFIC, AreaType::URBAN, 'uba_de', 'Kiel-Theodor-Heuss-Ring', null, new \DateTime('2012-01-01'), null);
+
+        $this->createStation(54.303894, 10.122691, 14, 'DESH029', StationType::TRAFFIC, AreaType::URBAN, 'uba_de', 'Kiel - Theodor-Heuss-Ring', null, new \DateTime('2006-05-17'), new \DateTime('2008-05-08'));
+
         $manager->flush();
     }
 
@@ -35,14 +51,3 @@ class StationFixtures extends Fixture
         return $station;
     }
 }
-
-
-/*
-1495	NULL	NULL	DESH019	Kiel-Westring Verk.	54.326394	10.115586	1995-04-26	2005-05-09	25	traffic	urban	uba_de
-1497	NULL	NULL	DESH021	Kiel Schauenburger Str.	54.332679	10.136176	2000-05-11	2006-11-30	10	background	urban	uba_de
-1503	NULL	NULL	DESH027	Kiel-Bahnhofstr. Verk.	54.304676	10.135158	2005-05-11	NULL	8	traffic	urban	uba_de
-1505	NULL	NULL	DESH029	Kiel - Theodor-Heuss-Ring	54.303894	10.122691	2006-05-17	2008-05-08	14	traffic	urban	uba_de
-1509	NULL	NULL	DESH033	Kiel-Max-Planck-Str.	54.309674	10.116563	2007-04-25	2017-06-22	29	background	urban	uba_de
-1528	NULL	NULL	DESH052	Kiel-Theodor-Heuss-Ring	54.304242	10.122364	2012-01-01	NULL	18	traffic	urban	uba_de
-1533	NULL	NULL	DESH057	Kiel-Bremerskamp	54.34761	10.1056	2017-06-22	NULL	30	background	urban	uba_de
-*/
