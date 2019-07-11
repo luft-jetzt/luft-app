cf1327135583364dd2d9c5e8b5d5af8b43830fc6
Malte Hübner <maltehuebner@gmx.org>
2019-07-11 08:06:41 +0200

    Introduce CO2.

---
+++ src/Air/Measurement/CO2.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace App\Air\Measurement;
+
+class CO2 extends AbstractMeasurement
+{
+    public function __construct()
+    {
+        $this->unitHtml = 'ppm';
+        $this->unitPlain = 'ppm';
+        $this->name = 'Kohlenstoffdioxid';
+        $this->shortNameHtml = 'CO<sub>2</sub>';
+    }
+}
---
+++ tests/Air/Measurement/CO2Test.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+namespace App\Tests\Air\Measurement;
+
+use App\Air\Measurement\CO2;
+use PHPUnit\Framework\TestCase;
+
+class CO2Test extends TestCase
+{
+    public function testName(): void
+    {
+        $this->assertEquals('Kohlenstoffdioxid', (new CO2())->getName());
+    }
+
+    public function testShortName(): void
+    {
+        $this->assertEquals('CO2', (new CO2())->getShortName());
+    }
+
+    public function testIdentifier(): void
+    {
+        $this->assertEquals('co2', (new CO2())->getIdentifier());
+    }
+
+    public function testShortNameHtml(): void
+    {
+        $this->assertEquals('CO<sub>2</sub>', (new CO2())->getShortNameHtml());
+    }
+
+    public function testUnitHtml(): void
+    {
+        $this->assertEquals('ppm', (new CO2())->getUnitHtml());
+    }
+
+    public function testUnitPlain(): void
+    {
+        $this->assertEquals('ppm', (new CO2())->getUnitPlain());
+    }
+}
