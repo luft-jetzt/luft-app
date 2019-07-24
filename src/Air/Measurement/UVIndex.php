dbb6a9cc5730dfe6e9096d99603f92d8f4a584f8
Malte Hübner <maltehuebner@gmx.org>
2019-07-24 18:06:15 +0200

    Add UVIndex measurement.

---
+++ src/Air/Measurement/UVIndex.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace App\Air\Measurement;
+
+class UVIndex extends AbstractMeasurement
+{
+    public function __construct()
+    {
+        $this->unitHtml = '';
+        $this->unitPlain = '';
+        $this->name = 'UV-Index';
+        $this->shortNameHtml = 'UV-Index';
+        $this->showOnMap = false;
+        $this->includeInTweets = false;
+    }
+}
