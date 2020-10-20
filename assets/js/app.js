1bd749ab29d06500116c58ea1bb019362a19726c
Malte Hübner <maltehuebner@gmx.org>
2020-10-20 19:53:02 +0200

    Extract geocoding as a new module.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 import 'bootstrap';
 import 'leaflet';
 import 'leaflet-extra-markers';
+import Geocoding from "./modules/Geocoding";
 
 function createAllMaps() {
     var cityMapId = $('.city-map').attr('id');
@@ -136,46 +137,6 @@ function createCoordMap(id) {
 
     markerGroup.addTo(map);
     map.fitBounds(markerGroup.getBounds(), { padding: [15, 15] });
-
-
-
-
-
-
-
-    function success(pos) {
-        var coords = pos.coords;
-
-        var route = Routing.generate(
-            'display',
-            {
-                latitude: coords.latitude,
-                longitude: coords.longitude
-            }
-        );
-
-        window.location = route;
-    }
-
-    function error(err) {
-        var $message = $('#geolocation-failed');
-
-        $message.removeClass('d-none');
-    }
-
-    function installButton() {
-        $('#locate-button').on('click', function () {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(success, error, {
-                    enableHighAccuracy: true,
-                    timeout: 5000,
-                    maximumAge: 0
-                });
-            } else {
-                error();
-            }
-        });
-    }
 }
 
 function initSearch()
@@ -306,6 +267,5 @@ function redirect(event, datum) {
 
 document.addEventListener('DOMContentLoaded', () => {
     createAllMaps();
-    installButton();
     initSearch();
 });
