4aa8f29d40641bc315a9ebd9fd8ba2007c5bef1e
Malte Hübner <maltehuebner@gmx.org>
2020-10-22 21:38:18 +0200

    More improvements.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,5 @@ import 'bootstrap';
 import Geocoding from './modules/Geocoding';
 import Map from './modules/Map';
 import Search from './modules/Search';
+
+export {Geocoding, Map, Search}
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,14 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-    // the project directory where compiled assets will be stored
     .setOutputPath('public/build/')
-    // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
-    // uncomment to create hashed filenames (e.g. app.abc123.css)
-    // .enableVersioning(Encore.isProduction())
-
+    .enableSingleRuntimeChunk()
     .addEntry('js/app', './assets/js/app.js')
     .addStyleEntry('css/app', './assets/scss/app.scss')
-
-    // uncomment if you use Sass/SCSS files
     .enableSassLoader()
-
-    // uncomment for legacy applications that require $/jQuery as a global variable
     .autoProvidejQuery()
     .copyFiles({
         from: './assets/img',
