85bdf2eecd15868270ca28da78475edad1279e57
Malte Hübner <maltehuebner@gmx.org>
2020-11-12 19:38:11 +0100

    Extract DataPurger from Command.

---
+++ src/DataPurger/DataPurger.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace App\DataPurger;
+
+use App\Entity\Data;
+use App\Provider\ProviderInterface;
+use Doctrine\Persistence\ManagerRegistry;
+
+class DataPurger implements DataPurgerInterface
+{
+    protected ManagerRegistry $managerRegistry;
+
+    public function __construct(ManagerRegistry $managerRegistry)
+    {
+        $this->managerRegistry = $managerRegistry;
+    }
+
+    public function purgeData(\DateTimeInterface $untilDateTime, ProviderInterface $provider = null): int
+    {
+        $dataList = $this->managerRegistry->getRepository(Data::class)->findInInterval(null, $untilDateTime, $provider);
+
+        $counter = count($dataList);
+
+        $em = $this->managerRegistry->getManager();
+
+        foreach ($dataList as $data) {
+            $em->remove($data);
+        }
+
+        $em->flush();
+
+        return $counter;
+    }
+}
