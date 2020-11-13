c813fb1703ad0516a588571b9d578bc346b5c90e
Malte Hübner <maltehuebner@gmx.org>
2020-11-13 23:18:16 +0100

    Show more details and datetime.

--- assets/js/modules/OverviewMap.js
+++ assets/js/modules/OverviewMap.js
@@ -22,6 +22,8 @@ export default class OverviewMap {
         Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
             return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
         });
+
+        Handlebars.registerHelper('dateFormat', require('handlebars-dateformat'));
     }
 
     createMap() {
