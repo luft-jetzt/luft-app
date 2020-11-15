1ba682d56a31e4610049fe4d31ad73192bb07505
Malte Hübner <maltehuebner@gmx.org>
2020-11-15 23:27:07 +0100

    WIP!

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,5 +2,6 @@ import 'bootstrap';
 import Geolocation from './modules/Geolocation';
 import Map from './modules/Map';
 import Search from './modules/Search';
+import TimeAgoInWords from './modules/TimeAgoInWords';
 
-export {Geolocation, Map, Search}
+export {Geolocation, Map, Search, TimeAgoInWords}
