7775fb91505a8e3bdcba66c7407c1b02bc195632
Malte Hübner <maltehuebner@gmx.org>
2020-12-23 02:34:56 +0100

    WIP.

--- src/Controller/AnalysisController.php
+++ src/Controller/AnalysisController.php
@@ -4,7 +4,13 @@ namespace App\Controller;
 
 use App\Analysis\FireworksAnalysis\FireworksAnalysisInterface;
 use App\Analysis\KomfortofenAnalysis\KomfortofenAnalysisInterface;
+use App\Pollution\PollutionDataFactory\PollutionDataFactoryInterface;
 use App\SeoPage\SeoPageInterface;
+use Caldera\GeoBasic\Coord\Coord;
+use Carbon\Carbon;
+use Carbon\CarbonInterval;
+use JMS\Serializer\Tests\Fixtures\Discriminator\Car;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Flagception\Bundle\FlagceptionBundle\Annotations\Feature;
 
@@ -53,8 +59,61 @@ class AnalysisController extends AbstractController
         ]);
     }
 
-    public function coronaFireworksAction(): Response
+    /**
+     * @Feature("analysis_fireworks")
+     */
+    public function coronaFireworksAction(Request $request, PollutionDataFactoryInterface $pollutionDataFactory): Response
     {
+        $latitude = $request->get('latitude');
+        $longitude = $request->get('longitude');
+
+        if (!$latitude || !$longitude) {
+            return $this->render('Analysis/corona_fireworks.html.twig');
+        }
+
+        $coord = new Coord((float) $latitude, (float) $longitude);
+
+        $yearList = $this->initYearList();
+
+        foreach ($yearList as $year => $hourList) {
+            foreach ($hourList as $timestamp => $data) {
+                $dateTime = Carbon::createFromTimestamp($timestamp);
+
+                $result = $pollutionDataFactory
+                    ->setCoord($coord)
+                    ->createDecoratedPollutantList($dateTime, new CarbonInterval('PT30M'), 1)
+                ;
+
+                $yearList[$year][$timestamp] = $result;
+            }
+        }
+
         return $this->render('Analysis/corona_fireworks.html.twig');
+    }
+
+    protected function initYearList(): array
+    {
+        $year = (new Carbon())->subDays(360);
+        $yearList = [];
+
+        for ($yearSub = 0; $yearSub <= 2; ++$yearSub) {
+            $yearList[$year->year] = [];
+            $year->subYear();
+        }
+
+        foreach ($yearList as $year => $hourList) {
+            $startDateTimeSpec = '%d-12-31 12:00:00';
+            $startDateTime = new Carbon(sprintf($startDateTimeSpec, $year));
+            $endDateTime = $startDateTime->copy()->addHours(36);
+
+            $dateTime = $endDateTime->copy();
+
+            do {
+                $yearList[$year][$dateTime->format('Y-m-d-H-i-00')] = null;
+                $dateTime->subMinutes(30);
+            } while ($dateTime > $startDateTime);
+        }
+
+        return $yearList;
     }
 }
