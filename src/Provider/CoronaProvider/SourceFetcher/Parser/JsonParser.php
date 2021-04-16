b3a0f75eda5d43e725e7dde8b657ab79c3e138d0
Malte Hübner <maltehuebner@gmx.org>
2021-04-17 00:05:56 +0200

    Fix timezones.

--- src/Provider/CoronaProvider/SourceFetcher/Parser/JsonParser.php
+++ src/Provider/CoronaProvider/SourceFetcher/Parser/JsonParser.php
@@ -11,9 +11,11 @@ class JsonParser implements JsonParserInterface
         $coronaValue = json_decode($jsonData);
 
         $value = new Value();
-        $value->setValue((float) $coronaValue->cases7_per100_k)
+        $value
+            ->setValue((float) $coronaValue->cases7_per100_k)
             ->setPollutant('coronaincidence')
-            ->setDateTime(new \DateTime($coronaValue->last_update, new \DateTimeZone('Europe/Berlin')));
+            ->setDateTime(new \DateTime($coronaValue->last_update, new \DateTimeZone('Europe/Berlin')))
+        ;
 
         return $value;
     }
