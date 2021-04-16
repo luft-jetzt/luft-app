acf6d9189752e882c22d06abd92512305a2fc1a5
Malte Hübner <maltehuebner@gmx.org>
2021-04-16 23:48:44 +0200

    Fetch corona incidence.

---
+++ src/Provider/CoronaProvider/SourceFetcher/Parser/JsonParser.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace App\Provider\CoronaProvider\SourceFetcher\Parser;
+
+use App\Pollution\Value\Value;
+
+class JsonParser implements JsonParserInterface
+{
+    public function parseCoronaIncidence(string $jsonData): Value
+    {
+        $coronaValue = json_decode($jsonData);
+
+        $value = new Value();
+        $value->setValue((float) $coronaValue->cases7_per100_k)
+            ->setPollutant('coronaincidence')
+            ->setDateTime(new \DateTime(sprintf('@%d', $coronaValue->last_update), new \DateTimeZone('UTC')));
+
+        return $value;
+    }
+}
