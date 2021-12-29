897a4d0a76f24044c398b9b17a0347c91b4833d7
Malte Hübner <maltehuebner@gmx.org>
2021-12-29 12:48:08 +0100

    Remove old stuff.

--- src/Analysis/CoronaFireworksAnalysis/Twig/CoronaFireworksTwigExtension.php
+++ src/Analysis/CoronaFireworksAnalysis/Twig/CoronaFireworksTwigExtension.php
@@ -10,16 +10,6 @@ use Twig\TwigFunction;
 
 class CoronaFireworksTwigExtension extends AbstractExtension
 {
-    public function getFilters(): array
-    {
-        return [
-            // If your filter generates SAFE HTML, you should add a third
-            // parameter: ['is_safe' => ['html']]
-            // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
-            new TwigFilter('filter_name', [$this, 'doSomething']),
-        ];
-    }
-
     public function getFunctions(): array
     {
         return [
@@ -30,6 +20,7 @@ class CoronaFireworksTwigExtension extends AbstractExtension
     public function coronaFireworksDateTime(string $minutesSinceStartDateTime): Carbon
     {
         $startDateTime = StartDateTimeCalculator::calculateStartDateTime(2020);
+
         return $startDateTime->addMinutes((int) $minutesSinceStartDateTime);
     }
 }
