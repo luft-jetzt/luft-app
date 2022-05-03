e0b899f5aaa002694e60d0091fce97732c7c9b1c
Malte Hübner <maltehuebner@gmx.org>
2022-05-03 23:31:19 +0200

    Fix even more config.

--- src/Kernel.php
+++ src/Kernel.php
@@ -16,9 +16,20 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+    const CONFIG_EXTS = '.{php,xml,yaml,yml}';
 
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader)
     {
+        $container->setParameter('container.autowiring.strict_mode', true);
+        $container->setParameter('container.dumper.inline_class_loader', true);
+        $confDir = $this->getProjectDir().'/config';
+
+        $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir.'/{packages}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir.'/services/*'.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS, 'glob');
+
         $container->addCompilerPass(new PollutionLevelCompilerPass());
         $container->registerForAutoconfiguration(PollutionLevelInterface::class)->addTag('pollution_level');
 
