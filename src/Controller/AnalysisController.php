4b3d42949855600f67c8d934be72c5b4396afb81
Malte Hübner <maltehuebner@gmx.org>
2022-12-24 02:34:51 +0100

    Convert analysis routing to attributes.

--- src/Controller/AnalysisController.php
+++ src/Controller/AnalysisController.php
@@ -12,6 +12,7 @@ use App\SeoPage\SeoPageInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Flagception\Bundle\FlagceptionBundle\Annotations\Feature;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Feature("analysis")
@@ -21,6 +22,7 @@ class AnalysisController extends AbstractController
     /**
      * @Feature("analysis_komfortofen")
      */
+    #[Route(path: '/analysis/komfortofen', name: 'analysis_komfortofen')]
     public function komfortofenAction(KomfortofenAnalysisInterface $komfortofenAnalysis, SeoPageInterface $seoPage): Response
     {
         $interval = new \DateInterval('P30D');
@@ -43,6 +45,7 @@ class AnalysisController extends AbstractController
     /**
      * @Feature("analysis_fireworks")
      */
+    #[Route(path: '/analysis/silvester', name: 'analysis_fireworks')]
     public function fireworksAction(FireworksAnalysisInterface $fireworksAnalysis, SeoPageInterface $seoPage): Response
     {
         $seoPage
@@ -61,6 +64,7 @@ class AnalysisController extends AbstractController
     /**
      * @Feature("analysis_fireworks")
      */
+    #[Route(path: '/analysis/corona-silvester', name: 'analysis_fireworks_corona')]
     public function coronaFireworksAction(Request $request, RequestConverterInterface $requestConverter, CoronaFireworksAnalysisInterface $coronaFireworksAnalysis): Response
     {
         $coord = $requestConverter->getCoordByRequest($request);
