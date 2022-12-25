b172950166814ff2196437957dd06ad4a3c459e9
Malte Hübner <maltehuebner@gmx.org>
2022-12-25 23:33:41 +0100

    WIP for handling everything in repositories.

--- src/Pollution/DataRetriever/PostgisDataRetriever.php
+++ src/Pollution/DataRetriever/PostgisDataRetriever.php
@@ -1,9 +1,9 @@
 <?php declare(strict_types=1);
 
 namespace App\Pollution\DataRetriever;
+
+use App\Entity\Data;
 use Caldera\GeoBasic\Coord\CoordInterface;
-use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\Statement;
 use Doctrine\Persistence\ManagerRegistry;
 
 class PostgisDataRetriever implements DataRetrieverInterface
@@ -15,17 +15,10 @@ class PostgisDataRetriever implements DataRetrieverInterface
 
     public function retrieveDataForCoord(CoordInterface $coord, int $pollutantId = null, \DateTime $fromDateTime = null, \DateInterval $dateInterval = null, float $maxDistance = 20.0, int $maxResults = 250): array
     {
-        /** @var Connection $connection */
-        $connection = $this->managerRegistry->getConnection();
+        $repository = $this->managerRegistry->getRepository(Data::class);
 
-        $sql = 'SELECT id, title, coord <-> ST_MakePoint(53.234211, 10.4104193) AS dist
-FROM station
-ORDER BY dist LIMIT 10';
+        $result = $repository->findCurrentDataForCoord($coord);
 
-        /** @var Statement $statement */
-        $statement = $connection->prepare($sql);
-        $result = $statement->executeQuery();
-
-        dd($result->fetchAllAssociative());
+        dd($result);
     }
 }
--- src/Repository/DataRepository.php
+++ src/Repository/DataRepository.php
@@ -5,217 +5,33 @@ namespace App\Repository;
 use App\Entity\Data;
 use App\Entity\Station;
 use App\Provider\ProviderInterface;
+use Caldera\GeoBasic\Coord\CoordInterface;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Statement;
 use Doctrine\ORM\EntityRepository;
 
 class DataRepository extends EntityRepository
 {
-    public function findLatestDataForStationAndPollutant(Station $station, int $pollutant, \DateInterval $dateInterval, \DateTime $fromDateTime = null, string $order = 'DESC'): ?Data
+
+    public function findCurrentDataForCoord(CoordInterface $coord): array
     {
-        $qb = $this->createQueryBuilder('d');
+        /** @var Connection $conn */
+        $connection = $this->getEntityManager()->getConnection();
+        $sql = '
+SELECT DISTINCT ON (d.pollutant) d.id, d.value, d.pollutant, d.date_time, s.id, s.title, s.coord <-> ST_MakePoint(:longitude, :latitude) AS dist
+FROM data AS d
+JOIN station AS s ON d.station_id = s.id 
+ORDER BY d.pollutant ASC, dist ASC, d.date_time DESC
+LIMIT 10';
 
-        $qb
-            ->where($qb->expr()->eq('d.station', ':station'))
-            ->andWhere($qb->expr()->eq('d.pollutant', ':pollutant'))
-            ->orderBy('d.dateTime', ':order')
-            ->setMaxResults(1)
-            ->setParameter('station', $station)
-            ->setParameter('pollutant', $pollutant)
-            ->setParameter('order', $order);
+        /** @var Statement $stmt */
+        $statement = $connection->prepare($sql);
 
-        if ($fromDateTime && $dateInterval) {
-            $qb
-                ->andWhere($qb->expr()->gt('d.dateTime', ':fromDateTime'))
-                ->andWhere($qb->expr()->lte('d.dateTime', ':untilDateTime'))
-                ->setParameter('fromDateTime', $fromDateTime)
-                ->setParameter('untilDateTime', $fromDateTime->add($dateInterval));
-        }
+        $params = [
+            'latitude' => $coord->getLatitude(),
+            'longitude' => $coord->getLongitude(),
+        ];
 
-        $query = $qb->getQuery();
-
-        return $query->getOneOrNullResult();
-    }
-
-    public function findInInterval(\DateTimeInterface $fromDateTime = null, \DateTimeInterface $untilDateTime = null, ProviderInterface $provider = null): array
-    {
-        $qb = $this->createQueryBuilder('d');
-
-        if ($fromDateTime) {
-            $qb
-                ->andWhere($qb->expr()->gte('d.dateTime', ':fromDateTime'))
-                ->setParameter('fromDateTime', $fromDateTime);
-        }
-
-        if ($untilDateTime) {
-            $qb
-                ->andWhere($qb->expr()->lte('d.dateTime', ':untilDateTime'))
-                ->setParameter('untilDateTime', $untilDateTime);
-        }
-
-        if ($provider) {
-            $qb
-                ->join('d.station', 's')
-                ->andWhere($qb->expr()->eq('s.provider', ':providerIdentifier'))
-                ->setParameter('providerIdentifier', $provider->getIdentifier());
-        }
-
-        $query = $qb->getQuery();
-
-        return $query->getResult();
-    }
-
-    public function findTaggedInInterval(\DateTimeInterface $fromDateTime = null, \DateTimeInterface $untilDateTime = null, ProviderInterface $provider = null, string $tag = null): array
-    {
-        $qb = $this->createQueryBuilder('d');
-
-        if ($fromDateTime) {
-            $qb
-                ->andWhere($qb->expr()->gte('d.dateTime', ':fromDateTime'))
-                ->setParameter('fromDateTime', $fromDateTime);
-        }
-
-        if ($untilDateTime) {
-            $qb
-                ->andWhere($qb->expr()->lte('d.dateTime', ':untilDateTime'))
-                ->setParameter('untilDateTime', $untilDateTime);
-        }
-
-        if ($provider) {
-            $qb
-                ->join('d.station', 's')
-                ->andWhere($qb->expr()->eq('s.provider', ':providerIdentifier'))
-                ->setParameter('providerIdentifier', $provider->getIdentifier());
-        }
-
-        $qb->andWhere($qb->expr()->isNotNull('d.tag'));
-
-        if ($tag) {
-            $qb
-                ->andWhere($qb->expr()->eq('d.tag', ':tag'))
-                ->setParameter('tag', $tag);
-        }
-
-        $query = $qb->getQuery();
-
-        return $query->getResult();
-    }
-
-    public function findUntaggedInInterval(\DateTimeInterface $fromDateTime = null, \DateTimeInterface $untilDateTime = null, ProviderInterface $provider = null, string $tag = null): array
-    {
-        $qb = $this->createQueryBuilder('d');
-
-        if ($fromDateTime) {
-            $qb
-                ->andWhere($qb->expr()->gte('d.dateTime', ':fromDateTime'))
-                ->setParameter('fromDateTime', $fromDateTime);
-        }
-
-        if ($untilDateTime) {
-            $qb
-                ->andWhere($qb->expr()->lte('d.dateTime', ':untilDateTime'))
-                ->setParameter('untilDateTime', $untilDateTime);
-        }
-
-        if ($provider) {
-            $qb
-                ->join('d.station', 's')
-                ->andWhere($qb->expr()->eq('s.provider', ':providerIdentifier'))
-                ->setParameter('providerIdentifier', $provider->getIdentifier());
-        }
-
-        $qb->andWhere($qb->expr()->isNull('d.tag'));
-
-        $query = $qb->getQuery();
-
-        return $query->getResult();
-    }
-
-    public function findHashsInterval(\DateTimeInterface $fromDateTime, \DateTimeInterface $untilDateTime, array $stationList = []): array
-    {
-        $sql = 'SELECT CONCAT(d.station_id, UNIX_TIMESTAMP(d.date_time), d.pollutant, d.value) AS hash FROM data AS d';
-
-        if (0 !== count($stationList)) {
-            $sql.= ' JOIN station AS s ON d.station_id = s.id';
-        }
-
-        $sql.= ' WHERE d.date_time >= \''.$fromDateTime->format('Y-m-d H:i:s').'\' AND d.date_time <= \''.$untilDateTime->format('Y-m-d H:i:s').'\'';
-
-        if (0 !== count($stationList)) {
-            $sql.= ' AND s.station_code IN (\''.implode('\', \'', $stationList).'\')';
-        }
-
-        $stmt = $this->getEntityManager()->getConnection()->prepare($sql);
-        $stmt->execute();
-
-        return $stmt->fetchAll();
-    }
-
-    public function findForAnalysis(Station $station, int $pollutant, \DateTimeInterface $fromDateTime = null, \DateTimeInterface $untilDateTime = null): array
-    {
-        $qb = $this->createQueryBuilder('d');
-
-        $qb
-            ->where($qb->expr()->eq('d.station', ':station'))
-            ->setParameter('station', $station)
-            ->andWhere($qb->expr()->eq('d.pollutant', ':pollutant'))
-            ->setParameter('pollutant', $pollutant)
-            ->leftJoin(
-                \App\Entity\Data::class,
-                'd2',
-                'WITH',
-                'd2.pollutant = d.pollutant AND d2.station = d.station AND d2.value > d.value'
-            )
-            ->andWhere($qb->expr()->isNull('d2.value'))
-            ->orderBy('d.dateTime','ASC');
-
-
-        $query = $qb->getQuery();
-
-        return $query->getResult();
-    }
-
-    public function deleteData(\DateTimeInterface $untilDateTime, ProviderInterface $provider = null, bool $tagged = false): int
-    {
-        if ($provider) {
-            $providerStationIdResult = $this->getEntityManager()->createQuery(
-                'SELECT s.id FROM App:Station s
-                WHERE s.provider = :provider'
-            )->setParameter('provider', $provider->getIdentifier())->execute();
-
-            $providerStationIdList = array_map(fn(array $stationRsult) => $stationRsult['id'], $providerStationIdResult);
-
-            if ($tagged) {
-                $query = $this->getEntityManager()->createQuery(
-                    'DELETE App:Data d 
-                    WHERE d.dateTime < :untilDateTime AND d.station IN (:stationIdList)')
-                    ->setParameter('untilDateTime', $untilDateTime)
-                    ->setParameter('stationIdList', $providerStationIdList)
-                ;
-            } else {
-                $query = $this->getEntityManager()->createQuery(
-                    'DELETE App:Data d 
-                    WHERE d.dateTime < :untilDateTime AND d.station IN (:stationIdList) AND d.tag IS NOT NULL')
-                    ->setParameter('untilDateTime', $untilDateTime)
-                    ->setParameter('stationIdList', $providerStationIdList)
-                ;
-            }
-
-            $result = $query->execute();
-        } elseif ($tagged) {
-            $query = $this->getEntityManager()->createQuery(
-                'DELETE App:Data d 
-                WHERE d.dateTime < :untilDateTime AND d.tag IS NOT NULL')
-                ->setParameter('untilDateTime', $untilDateTime);
-
-            $result = $query->execute();
-        } else {
-            $query = $this->getEntityManager()->createQuery(
-                'DELETE App:Data d 
-                WHERE d.dateTime < :untilDateTime AND d.tag IS NULL')
-                ->setParameter('untilDateTime', $untilDateTime);
-
-            $result = $query->execute();
-        }
-
-        return $result;
+        return $statement->executeQuery($params)->fetchAllAssociative();
     }
 }
