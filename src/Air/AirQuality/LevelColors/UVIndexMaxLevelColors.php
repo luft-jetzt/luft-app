d4f1e0ed155324bde9df099747782fb34e9eb773
Malte Hübner <maltehuebner@gmx.org>
2023-06-15 17:48:51 +0200

    Minor changes.

--- src/Air/AirQuality/LevelColors/UVIndexMaxLevelColors.php
+++ src/Air/AirQuality/LevelColors/UVIndexMaxLevelColors.php
@@ -2,21 +2,7 @@
 
 namespace App\Air\AirQuality\LevelColors;
 
-class UVIndexMaxLevelColors extends AbstractLevelColors
+class UVIndexMaxLevelColors extends UVIndexLevelColors
 {
-    protected array $backgroundColors = [
-        1 => '#28a745',
-        2 => '#ffc107',
-        3 => '#f87820',
-        5 => '#dc3545',
-        4 => '#a80080',
-    ];
 
-    protected array $backgroundColorNames = [
-        1 => 'green',
-        2 => 'yellow',
-        3 => 'orange',
-        4 => 'red',
-        5 => 'purple',
-    ];
 }
