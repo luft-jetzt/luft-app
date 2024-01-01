e148a97978e9ad55ddfb9f7baaf6b0a078df7cfa
Malte Hübner <maltehuebner@gmx.org>
2024-01-01 14:01:06 +0100

    Cleanup.

--- rector.php
+++ rector.php
@@ -3,9 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Doctrine\Set\DoctrineSetList;
-use Rector\Symfony\Set\SymfonySetList;
-use Rector\Symfony\Set\JMSSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
