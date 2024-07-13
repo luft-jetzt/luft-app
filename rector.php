2cac78c7944796929cef6e3d12f490f6277f65c3
Malte Hübner <maltehuebner@gmx.org>
2024-07-13 13:30:36 +0200

    Clear rector config.

--- rector.php
+++ rector.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
+use Rector\Symfony\Set\SymfonySetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -11,6 +11,5 @@ return static function (RectorConfig $rectorConfig): void {
     ]);
 
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_83,
     ]);
 };
